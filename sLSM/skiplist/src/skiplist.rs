60f73f5e89d9efc6b06715087c0a3803b599ec5e
wellls <wellschuan@gmail.com>
2020-06-08 01:51:23 +0800

    Skiplist: define skiplist methods.

--- sLSM/skiplist/src/skiplist.rs
+++ sLSM/skiplist/src/skiplist.rs
@@ -24,9 +24,74 @@ impl <K, V> SkipList<K, V>
             level_gen: lg,
         }
     }
+
+    #[inline]
+    pub fn with_capacity(capacity: usize) -> Self {}
+
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        unsafe {}
+    }
+
+    pub fn delete_key(&mut self, key: K) -> Self {
+
+    }
+
+    pub fn loopup(&mut self, key: K, found: bool) -> Option<V> {
+
+    }
+
+    pub fn get_all(&mut self) -> Vec<Option<K, V>> {
+
+    }
+
+    pub fn get_all_in_range(&mut self, key1: K, key2: K) -> Vec<Option<K, V>> {
+
+    }
+
 }
 
-#[inline]
-pub fn with_capacity(capacity: usize) -> Self {
+impl <K, V> SkipList<K, V> {
+    #[inline]
+    pub fn clear(&mut self) {
+        unsafe {
+
+        }
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        // self.length
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.length == 0
+    }
+
+
+    #[inline]
+    pub fn front(&self) -> Option<(&K, &V)> {
+
+    }
+
+    #[inline]
+    pub fn elt_in(&self, key: K) -> bool {
+
+    }
+
+    #[inline]
+    pub fn get_min(&self) -> Option<&K> {
+
+    }
+
+    #[inline]
+    pub fn get_max(&self) -> Option<&K> {
+
+    }
+
+    #[inline]
+    pub fn get_size_bytes(&self) -> usize {
+
+    }
 
 }

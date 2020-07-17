3e4d3027c5d4d59e398639d7539e0003cbe6bc57
wellls <i@eventloop.live>
2020-07-17 18:05:53 +0800

    sLSM: create disk_run struct.

---
+++ sLSM/disk/src/disk_run.rs
@@ -0,0 +1,18 @@
+use std::cmp::PartialEq;
+
+
+pub struct DiskRun<K, V> {
+
+    pub KVpair_t 
+}
+
+pub trait DiskRun<K, V> {
+
+    fn new() -> Self;
+
+    fn compare_kvs(&self, key: K, value: V) -> i64;
+    fn set_capacity(&self, newCap: usize);
+
+
+}
+

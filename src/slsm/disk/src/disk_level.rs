aa8fe0fc9d036b30b204ba64e35b4da526f11482
wellls <wellschuan@gmail.com>
2020-07-31 23:32:40 +0800

    slsm disk: redefine disk level and disk run struct.

--- src/slsm/disk/src/disk_level.rs
+++ src/slsm/disk/src/disk_level.rs
@@ -1,19 +1,27 @@
-use std::iter;
-use std::collections::BinaryHeap;
+// use std::collections::BinaryHeap;
+// use std::iter;
 
 use skiplist::run::KVpair;
-
-
+use crate::disk_run::DiskRun;
 
 #[derive(Debug, Clone)]
 pub struct KVIntPairT<K, V> {
-  pub kvpair: KVpair<K, V>,
-  pub i: isize,
+    pub kvpair: KVpair<K, V>,
+    pub i: isize,
 }
 
-// pub struct DiskLevel<K, V> {
-// 
-// 
-// }
+pub struct DiskLevel<K, V> {
+    tombstone:      isize,
+    KVINTPAIRTMAX:  isize,
+    KVPAIRMAX:      isize,
 
-
+    pub level:      isize,
+    pub page_size:  usize,
+    pub run_size:   usize,
+    pub run_nums:   usize,
+    pub active_run: usize,
+    pub merge_size: usize,
+    pub bf_fp:      f64,
+    pub runs:       Vec<DiskRun<K, V>>,
+    
+}
--- src/slsm/disk/src/disk_run.rs
+++ src/slsm/disk/src/disk_run.rs
@@ -1,19 +1,18 @@
-// use std::cmp::PartialEq;
-// 
-// 
-// pub struct DiskRun<K, V> {
-// 
-//     pub KVpair_t 
-// }
-// 
-// pub trait DiskRun<K, V> {
-// 
-//     fn new() -> Self;
-// 
-//     fn compare_kvs(&self, key: K, value: V) -> i64;
-//     fn set_capacity(&self, newCap: usize);
-// 
-// 
-// }
-// 
-// 
+use skiplist::run::KVpair;
+
+
+pub struct DiskRun<K, V> {
+    pub fd:         isize,
+    pub page_size:  isize,
+    pub min_key:    isize,
+    pub max_key:    isize,
+    pub map:        KVpair<K, V>,
+
+    capacity:       usize,
+    filename:       String,
+    level:          isize,
+    fence_pointers: Vec<K>,
+    imax_fp:        usize,
+    run_id:         usize,
+    bf_fp:          f64,
+}

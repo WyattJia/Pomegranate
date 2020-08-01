e323d60feaf710e1d7f23b8bdc649818dc4c4ca4
wellls <wellschuan@gmail.com>
2020-08-01 23:58:07 +0800

    slsm disk: define disk run methods.

--- src/slsm/disk/src/disk_run.rs
+++ src/slsm/disk/src/disk_run.rs
@@ -1,3 +1,5 @@
+use std::cmp:Ord;
+
 use skiplist::run::KVpair;
 
 
@@ -15,4 +17,76 @@ pub struct DiskRun<K, V> {
     imax_fp:        usize,
     run_id:         usize,
     bf_fp:          f64,
+}
+
+// impl <K: Ord, V: Ord> for DiskRun<K, V>
+// {
+//     fn compareKVs(&self, a: &KVpair<K, V>, b: &KVpair<K, V>) -> isize {
+//         /*
+//          * Compare KV pairs.
+//          */
+//         return 10
+//     }
+// 
+// 
+// }
+
+impl <K, V> for DiskRun<K, V>
+{
+    fn new(capacity: usize, page_size: usize, page_size: isize) -> Self {
+        let _filename = "C_" + level
+    }
+
+    fn set_capacity(&mut self, new_capacity: usize) {
+        &mut self.capacity = new_capacity;
+    }
+
+    fn get_capacity(&self) -> usize {
+        return &self.capacity
+    }
+
+    fn write_data(&mut self, run: KVpair<K, V>, offset: usize, len: usize){
+        &mut self.capacity = len;
+    }
+
+    fn construct_index(&mut self){
+
+    }
+
+    fn binary_search(&mut self, offset: usize, n: usize, &key: K, &found: bool) -> usize {
+        let mut min = offset;
+        min
+    }
+
+    fn get_flanking_fp(&mut self, &start: usize, &end: usize){
+
+    }
+
+    fn get_index(&mut self, &key: K, &found: bool) -> usize {
+
+    }
+
+    fn lookup(&self, &key: K, &found: bool) -> V {
+
+    }
+
+    fn range(&self, &key1: K, key2: K, &i1: usize, &i2: usize){
+
+    }
+
+    fn print_elts(&self){
+
+    }
+
+    fn do_map(&mut self) {
+
+    }
+
+    fn do_unmap(&mut self) {
+
+    }
+
+    fn double_size(&mut self) {
+
+    }
 }

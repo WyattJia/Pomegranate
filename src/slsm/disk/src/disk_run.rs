6b264aa8bb4b928c1b5af0680d2b2167131aed99
wellls <wellschuan@gmail.com>
2020-08-04 02:28:00 +0800

    slsm disk: fix open file err and require 3rd crate.

--- src/slsm/disk/src/disk_run.rs
+++ src/slsm/disk/src/disk_run.rs
@@ -1,5 +1,9 @@
-use std::fs::File;
+use std::path::Path;
 use std::cmp::Ord;
+use std::fs::File;
+
+use nix;
+use tempfile;
 
 use skiplist::run::KVpair;
 
@@ -35,13 +39,42 @@ pub struct DiskRun<K, V> {
 impl <K, V> DiskRun<K, V>
 {
     fn new(capacity: usize, page_size: usize, level: isize, run_id: isize, bf_fp: f32) -> Self {
+        
         let _filename = "C_".to_owned() + &level.to_string() + "_" + &run_id.to_string() + ".txt";
+        let mut result:i62;
 
-        let mut result:i64;
 
-        let fd =     
-        let mut file = File::open("foo.txt")?;
+        let tempdir = tempfile::tempdir().unwrap();
+        let fullpath = tempdir.path().join(&_filename);
+        drop(File::create(&fullpath).unwrap());
+        
+        let path = Path::new(&_filename);
+        let display = path.display();
 
+        let mut fd = match nix::fcntl::open(&fullpath, nix::fcntl::OFlag::empty(), nix::sys::stat::Mode::empty()) {
+            Err(why) => panic!("couldn't open {}: {}", display, why),
+            Ok(fd) => fd,
+        };
+
+
+        /*
+           * Stretch the file size to the size of the (mmapped) array of KVPairs
+           * 扩展文件大小
+           * lseek 重新定位文件的读写位置。
+           * 每一个已打开的文件都有一个读写位置, 当打开文件时通常其读写位置是指向文件开头,
+           * 若是以附加的方式打开文件(如O_APPEND), 则读写位置会指向文件尾.
+           * 当read()或write()时, 读写位置会随之增加,lseek()便是用来控制该文件的读写位置.
+           * 参数fildes 为已打开的文件描述词, 参数offset 为根据参数whence来移动读写位置的位移数.
+           
+        result = lseek(fd, filesize - 1, SEEK_SET);
+        if (result == -1) {
+            close(fd);
+            perror("Error calling lseek() to 'stretch' the file");
+            exit(EXIT_FAILURE);
+        }
+
+        result = nix::unistd::lseek(fd, )
+        */
     }
 
     fn set_capacity(&mut self, new_capacity: usize) {

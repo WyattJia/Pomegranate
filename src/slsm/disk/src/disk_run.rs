8fe6e7d32529603a3c89872924ede6a45264d095
wellls <wellschuan@gmail.com>
2020-08-10 02:29:11 +0800

    slsm disk run: change map field kvpair type to hashmap.

--- src/slsm/disk/src/disk_run.rs
+++ src/slsm/disk/src/disk_run.rs
@@ -1,13 +1,14 @@
-use std::path::Path;
-use std::fs::File;
+use std::convert::TryInto;
 use std::fs::remove_file;
+use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::{Seek, SeekFrom, Write};
-use std::os::unix::prelude::AsRawFd;
-use std::ptr;
-use std::convert::TryInto;
 use std::iter;
 use std::mem;
+use std::collections::HashMap;
+use std::os::unix::prelude::AsRawFd;
+use std::path::Path;
+use std::ptr;
 
 use libc;
 use nix;
@@ -15,21 +16,20 @@ use tempfile;
 
 use skiplist::run::KVpair;
 
+pub struct DiskRun<'a, K: 'a, V: 'a> {
+    pub fd: isize,
+    pub page_size: isize,
+    pub min_key: isize,
+    pub max_key: isize,
+    pub map: HashMap<&'a str, KVpair<K, V>>,
 
-pub struct DiskRun<K, V> {
-    pub fd:         isize,
-    pub page_size:  isize,
-    pub min_key:    isize,
-    pub max_key:    isize,
-    pub map:        KVpair<K, V>,
-
-    capacity:       usize,
-    filename:       String,
-    level:          isize,
+    capacity: usize,
+    filename: String,
+    level: isize,
     fence_pointers: Vec<Option<*mut K>>,
-    imax_fp:        usize,
-    run_id:         usize,
-    bf_fp:          f64,
+    imax_fp: usize,
+    run_id: usize,
+    bf_fp: f64,
 }
 
 // impl <K: Ord, V: Ord> for DiskRun<K, V>
@@ -40,24 +40,22 @@ pub struct DiskRun<K, V> {
 //          */
 //         return 10
 //     }
-// 
-// 
+//
+//
 // }
 
-impl <K, V> DiskRun<K, V>
-{
+impl<'a, K, V> DiskRun<'a, K, V> {
     fn new(capacity: usize, page_size: usize, level: isize, run_id: isize, bf_fp: f32) -> Self {
-
         let size = 1024 * 1024;
         let _filename = "C_".to_owned() + &level.to_string() + "_" + &run_id.to_string() + ".txt";
         let mut f = OpenOptions::new()
-                .read(true)
-                .write(true)
-                .create(true)
-                .open(&_filename)
-                .expect("unable to open file.");
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&_filename)
+            .expect("unable to open file.");
 
-        let mut result:i64;
+        let mut result: i64;
 
         // allocate space in the file first.
         f.seek(SeekFrom::Start(size as u64)).unwrap();
@@ -74,19 +72,17 @@ impl <K, V> DiskRun<K, V>
                 0,
             );
 
-            
             if c_void_map == libc::MAP_FAILED {
                 panic!("Could not access data from memory mapped file.")
             };
-            
+
             // ptr::copy_nonoverlapping(&_filename, map as *mut String, _filename.len());
 
-            let mut mut_map: &mut KVpair<K, V> = &mut *(c_void_map as *mut KVpair<K, V>);
-
+            let mut mut_map: &mut HashMap<KVpair<K, V>> = &mut *(c_void_map as *mut KVpair<K, V>);
 
             // todo get this fucking transmute_copy away.
             let map = mem::transmute_copy(mut_map);
-           
+
             /* Todo: review mmap usage.
             map = (KVPair<K, V>*) mmap(0, filesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
@@ -101,81 +97,83 @@ impl <K, V> DiskRun<K, V>
             let tempdir = tempfile::tempdir().unwrap();
             let fullpath = tempdir.path().join(&_filename);
             drop(File::create(&fullpath).unwrap());
-        
+
             let path = Path::new(&_filename);
             let display = path.display();
 
-            let fd = match nix::fcntl::open(&fullpath, nix::fcntl::OFlag::empty(), nix::sys::stat::Mode::empty()) {
+            let fd = match nix::fcntl::open(
+                &fullpath,
+                nix::fcntl::OFlag::empty(),
+                nix::sys::stat::Mode::empty(),
+            ) {
                 Err(why) => panic!("couldn't open {}: {}", display, why),
                 Ok(fd) => fd,
             };
-
 
             DiskRun {
                 fd: fd as isize,
                 filename: _filename,
                 min_key: 0,
                 max_key: 0,
-                map: map, 
+                map: map,
                 capacity: capacity,
                 page_size: page_size as isize,
                 level: level,
-                fence_pointers:  iter::repeat(None).take(level.try_into().unwrap()).collect(), 
+                fence_pointers: iter::repeat(None).take(level.try_into().unwrap()).collect(),
                 imax_fp: 0,
                 run_id: 0,
                 bf_fp: 0.0,
             }
-
-
         }
-
     }
 
     #[inline]
     fn set_capacity(&mut self, new_capacity: usize) {
         self.capacity = new_capacity;
     }
-    
+
     #[inline]
     fn get_capacity(self) -> usize {
-        return self.capacity
+        return self.capacity;
     }
 
-    fn write_data(&mut self, run: &mut KVpair<K, V>, offset: usize, len: usize){
+    fn write_data(&mut self, run: &mut KVpair<K, V>, offset: usize, len: usize) {
         // todo cover self.map to String
         unsafe {
-            ptr::copy_nonoverlapping( &self.map as *const KVpair<K, V>,  run, &self.filename.len() + offset);
+            ptr::copy_nonoverlapping(
+                &self.map as *const KVpair<K, V>,
+                run,
+                &self.filename.len() + offset,
+            );
         }
         self.capacity = len
-
     }
 
-    fn construct_index(&mut self){
+    fn construct_index(&mut self) {
+        /*
+                // construct fence pointers and write BF
+                //        _fencePointers.resize(0);
+                _fencePointers.reserve(_capacity / pageSize);
+                _iMaxFP = -1; // TODO IS THIS SAFE?
+                for (int j = 0; j < _capacity; j++) {
+                    bf.add((K*) &map[j].key, sizeof(K));
+                    if (j % pageSize == 0){
+                        _fencePointers.push_back(map[j].key);
+                        _iMaxFP++;
+                    }
+                }
+                if (_iMaxFP >= 0){
+                    _fencePointers.resize(_iMaxFP + 1);
+                }
 
-/*
-        // construct fence pointers and write BF
-        //        _fencePointers.resize(0);
-        _fencePointers.reserve(_capacity / pageSize);
-        _iMaxFP = -1; // TODO IS THIS SAFE?
-        for (int j = 0; j < _capacity; j++) {
-            bf.add((K*) &map[j].key, sizeof(K));
-            if (j % pageSize == 0){
-                _fencePointers.push_back(map[j].key);
-                _iMaxFP++;
-            }
-        }
-        if (_iMaxFP >= 0){
-            _fencePointers.resize(_iMaxFP + 1);
-        }
-
-        minKey = map[0].key;
-        maxKey = map[_capacity - 1].key;
-*/
+                minKey = map[0].key;
+                maxKey = map[_capacity - 1].key;
+        */
         // self.fence_pointers.reverse(self.capacity / self.page_size as usize);
         self.fence_pointers.reverse();
-        
+
         let mut max_fp = -1;
-        let mut j:usize = 0;
+        let mut j: usize = 0;
         while j < self.capacity {
             j += 1;
             // todo init bloom
@@ -184,7 +182,6 @@ impl <K, V> DiskRun<K, V>
                 self.fence_pointers.push(self.map[j].key);
                 max_fp += 1;
             }
-
         }
 
         if max_fp >= 0 {
@@ -194,74 +191,60 @@ impl <K, V> DiskRun<K, V>
         // todo change map type to hash or vec , check cpp's type
         self.min_key = map[0].key;
         self.max_key = map[self.capacity - 1].key;
-
     }
 
     fn binary_search(&mut self, offset: usize, n: usize, key: &K, found: bool) -> usize {
-        let  min = offset;
+        let min = offset;
 
         while n == 0 {
-             found = true;
-             return offset
-        }; 
+            found = true;
+            return offset;
+        }
 
         let mut min = offset;
         let mut max = offset + n - 1;
         let mut middle = (min + max) >> 1;
         while (min <= max) {
-            if key > map[middle].key {
-                
+            if (key > map[middle].key) {
+                min = middle + 1;
             } else if (key == map[middle].key) {
                 found = true;
-                return 
+                return middle;
+            } else {
+                max = middle - 1;
+                middle = (min + max) >> 1;
             }
         }
 
-        
+        return min;
     }
 
-    fn get_flanking_fp(&mut self, start: &usize, end: &usize){
-
-    }
+    fn get_flanking_fp(&mut self, start: &usize, end: &usize) {}
 
     fn get_index(&mut self, key: &K, found: &bool) -> usize {
-
-        let i:usize = 1;
+        let i: usize = 1;
         i
     }
 
     fn lookup(&self, key: &K, found: &bool) -> Option<&V> {
-
-       None 
+        None
     }
 
-    fn range(&self, key1: &K, key2: &K, i1: &usize, i2: &usize){
+    fn range(&self, key1: &K, key2: &K, i1: &usize, i2: &usize) {}
 
-    }
-
-    fn print_elts(&self){
-
-
+    fn print_elts(&self) {
         let mut j: usize = 0;
         while j < self.capacity {
             j += 1
         }
-
     }
 
-    fn do_map(&mut self) {
+    fn do_map(&mut self) {}
 
-    }
+    fn do_unmap(&mut self) {}
 
-    fn do_unmap(&mut self) {
-
-    }
-
-    fn double_size(&mut self) {
-
-    }
+    fn double_size(&mut self) {}
 }
-
 
 impl<K, V> Drop for DiskRun<K, V> {
     #[inline]
@@ -269,13 +252,8 @@ impl<K, V> Drop for DiskRun<K, V> {
         nix::unistd::fsync(*&self.fd as i32);
         &self.do_unmap();
 
-        if let Err(e) = remove_file(&self.filename) {                        
-            panic!(                                                           
-                "failed to remove file, maybe file race? {}",
-                e                                                            
-            );                                                               
+        if let Err(e) = remove_file(&self.filename) {
+            panic!("failed to remove file, maybe file race? {}", e);
         };
-
-
     }
 }
